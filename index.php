ca81b62834bbe2a65eaaa706c16b008fe1895fe3
christocarr <christocarr@outlook.com>
2019-02-21 21:17:05 +0000

    Add wp_nav_menu to nav tag

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+  <nav class="main-navigation" role="navigation">
+    <?php wp_nav_menu( [
+      'theme_location' => 'main-menu'
+    ]); ?>
+  </nav>
+  <main role="main">
+  </main>
+  <?php get_footer(); ?>
