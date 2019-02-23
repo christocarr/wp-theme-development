8e81422db52fdf9f288090d3122289d12767aa65
christocarr <christocarr@outlook.com>
2019-02-23 11:43:28 +0000

    Add get_sidebar

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php get_header(); ?>
-  <main role="main">
-  </main>
+
+  <div id="primary" class="content-area">
+
+    <main class="site-main" role="main">
+      <div class="content-entry">
+        <h1>index.php</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi iusto minus, reiciendis cumque numquam ipsam ducimus, magnam rerum quidem neque distinctio officiis quia, porro ad perferendis! Quaerat minus autem enim!</p>
+      </div>
+    </main>
+
+  </div>
+  
+  <?php get_sidebar(); ?>
+
   <?php get_footer(); ?>
