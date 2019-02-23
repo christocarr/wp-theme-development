103566e5d4621a4f236352556736a2df1b8a363f
christocarr <christocarr@outlook.com>
2019-02-23 14:55:16 +0000

    Add bulma link to footer

--- footer.php
+++ footer.php
@@ -1,8 +1,16 @@
-      <footer role="footer">
-        <p class="copyright">copyright</p>
-        <?php wp_nav_menu( [
-          'theme_location' => 'footer-menu'
-        ]); ?>
+      <footer class="site-footer footer level" role="contentinfo">
+        <a class="level-item has-text-centered" href="<?php echo esc_url( __( 'http://wordpress.org/', 'picturesandpaintings' ) ); ?>">
+          <?php printf( esc_html__( 'Powered by %s', 'picturesandpaintings' ), 'Wordpress'); ?>
+        </a>
+        <a class="level-item has-text-centered" href="<?php echo esc_url( __( 'https://bulma.io/', 'picturesandpaintings' ) ); ?>">
+          <?php printf( esc_html__( 'Bulma', 'picturesandpaintings' ), 'Bulma'); ?>
+        </a>
+        <p class="copyright level-item has-text-centered">copyright</p>
+        <div class="level-item has-text-centered">
+          <?php wp_nav_menu( [
+            'theme_location' => 'footer-menu'
+          ]); ?>
+        </div>
       </footer>
       <script></script>
       <?php wp_footer(); ?>
