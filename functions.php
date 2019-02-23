b7c384f1efcebb4ae0d99effc033469883762965
christocarr <christocarr@outlook.com>
2019-02-23 10:13:08 +0000

    Add bulma

--- functions.php
+++ functions.php
@@ -16,10 +16,11 @@
   add_theme_support( 'custom-logo' );
 
   //load CSS
-  function wphierarchy_enqueue_styles() {
+  function picturesandpaintings_enqueue_styles() {
+    wp_enqueue_style( 'bulma', 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css' );
     wp_enqueue_style( 'main-css', get_stylesheet_directory_uri() . '/style.css',[], 1.0  );
   }
-  add_action( 'wp_enqueue_scripts', 'wphierarchy_enqueue_styles' );
+  add_action( 'wp_enqueue_scripts', 'picturesandpaintings_enqueue_styles' );
 
   register_nav_menus( [
     'main-menu' => esc_html__( 'Main Menu', 'wpheirarchy' ),
