4243678418ff0e06166d477df9da269bd2faa644
christocarr <christocarr@outlook.com>
2019-02-23 14:57:38 +0000

    Edit nav to be main container

--- header.php
+++ header.php
@@ -14,22 +14,28 @@
     <body <?php body_class(); ?> >
       
     <header id="masthead" class="site-header" role="banner">
+      
+      <nav class="main-navigation navbar" role="navigation">
+        <div class="container">
 
-      <div class="site-branding">
-        <p class="site-title">
-          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-            <?php bloginfo( 'name' ); ?>
-          </a>
-        </p>
-        <p class="site-description">
-          <?php bloginfo( 'description' ); ?>
-        </p>
-      </div>
+          <div class="site-branding navbar-brand">
+            <p class="site-title">
+              <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                <?php bloginfo( 'name' ); ?>
+              </a>
+            </p>
+            <p class="site-description">
+              <?php bloginfo( 'description' ); ?>
+            </p>
+          </div>
 
-      <nav class="main-navigation" role="navigation">
-        <?php wp_nav_menu( [
-          'theme_location' => 'main-menu'
-        ]); ?>
+          <div class="nav-menu">
+            <?php wp_nav_menu( [
+              'theme_location' => 'main-menu'
+            ]); ?>
+          </div>
+
+        </div>
       </nav>
 
     </header>
